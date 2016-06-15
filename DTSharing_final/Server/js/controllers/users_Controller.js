38599b6dbe851616ae3bb5020ae60a6814c81bdb
Johannes-Kutsch <johanneskutsch@live.de>
2016-06-15 14:26:50 +0200

    .

--- DTSharing_final/Server/js/controllers/users_Controller.js
+++ DTSharing_final/Server/js/controllers/users_Controller.js
@@ -1,5 +1,6 @@
 var Users = require('../models/users'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    default_picture = 'iVBORw0KGgoAAAANSUhEUgAAAMgAAAEsBAMAAAB01OGNAAAAIVBMVEX19fXd3d3n5+fi4uLl5eXp6enz8/Px8fHv7+/t7e3r6+vwMGzcAAAD0UlEQVR42u3bz08TQRQH8BFxC5x43aWlnFqUiycG0SAnqkHwVgSi8VR/ofFUoqDealAOnBYTkHhq/RHDfymHwktL29nuzHel5H3+AF6YN/PezNtUCSGEEEIIIYQQQgghhBCif3h7NzSduLX4XaFsFenMzKFCSK1Sk4fKvaEitZiHxEBH8YrUxl3lVJ3aWlIOvaQOQocJ0dRBGrhY7I5y5Bp15leUGwXqIqucGKSuKrh/hGWBGWFl3NZi0w7OCJkEytoVMqopW0ViqNQPk5mvLD2jCEq41WIZy8ZOUQSOSgqytGxSJAv2KQFvYo+iSQOLI1MWrlJEoV3e8ZlfoYhGcZuLjeM2FxuzbFjowjJCDFbtByiyMq48siruLLIwiSA1XO9lJVxVYXkJcomC9LK7LnYQi8MICVJNokBWcDcihnucMN+ux+OvkJoiytnOCdDPoH2KaBr6vuaziD8oFctxBP7RqHGbi9XBeefMo6crI8CiwjQuJewzLiVsEHdKmAd8ZrF93GqxAeADiBVxL1/2AncSmQdMO9u0vQXbjwjTyo13uEkq8zR1NKEYrLZUE/io9cB6lT79WQ/581y37bu1frwbK8RPTUR+2O3qHTTK73s6Mfuk97t2sfXvnOeHze3zdo8l/5VuXZEv52PUWtcyqMZt7XOn696SlyDkQWWcKEO63XEbvk7EFk/XZifm+S80L0tVNXy9Tw33DlXDc2oyF7e4p7krvf6xPjn59GibN4iON/hK6egf91NFU+WP3qWWOx2mFUPFNJZDcxRv1VAye5wQzJfbbMICtRPGvzjMnDsCbzW1lbW4YvvNtcn7TR34Vh195m/5LMSBtun6mrqYnTra3tj4sLemrS5iI2TPt5qjuZqwFciBjPVLwX6cM0hOlHEpYXlcSlgGlxI2hjslzDdUYDdC86vd3oIh7/DMa3IkZzVDs5+yXSNngJuLVZFFxVxY9smZadwOZhncDmY5YHk0l8hhcicAHhMG7L2sgjuLLMSdRVZCdhNTR6mTA6Yjv0IOZczPH3tZXFVhuSSCTCBLl6F4pcilNK4+sqCcQBC/ArwIcxBkpe9+XxnowyAhrmex2qUJUmrffSXIfwySlyAXLkj7xA/2YZBaov0k+c44RC4FCd278Hfh8SRu9aPmbwH28km8GasJvOP9ZMce+CkRfqiGfqEEqrM6YLVw1Z6rI66yjCfxkSYEf9PiQQF2/mz+6fQOWSspo19kaUmZeZZRllUk37RFPh6riN6sxQwze7Oiovt48GiyZ1PHu0oIIYQQQgghhBBCCCGEEEKIvvQPzSbKEl1V5+cAAAAASUVORK5CYII=';
     
 module.exports.register = function (req, res) {
     Users.findOne({email : req.body.email}, function(err, result) {
@@ -28,7 +29,7 @@ module.exports.register = function (req, res) {
             more: req.body.more,
             email: req.body.email,
             pass: req.body.pass,
-            picture: null,
+            picture: default_picture,
             picture_version: 0
             });
             user.save(function (err, result) {
